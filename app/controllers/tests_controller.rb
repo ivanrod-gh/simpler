3097ec3fddd19924ae632a6f5b07c03ad7d96972
Ivan Rodionov <uhf_job@mail.ru>
2023-04-28 23:13:32 +0300

    Add new response types and logger

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,21 @@
 class TestsController < Simpler::Controller
 
   def index
+    # render 'tests/list.html'
+    # render 'tests/index.text'
+    # render 'tests/index.html'
+    # render 'tests/index'
+    # render template: "tests/index.text"
+    # render template: "tests/index.text", status: 234
+    # render plain: "plain text"
+    # render html: %Q[<h1>html <span style="color: red;">answer</span></h1>]
     @time = Time.now
   end
 
-  def create
+  def create; end
 
+  def show
+    @id = params[:id]
   end
 
 end
--- lib/simpler/router/route.rb
+++ lib/simpler/router/route.rb
@@ -11,10 +11,36 @@ module Simpler
         @action = action
       end
 
-      def match?(method, path)
-        @method == method && path.match(@path)
+      def match?(method, path, env)
+        if @path.include?(':')
+          method == @method && compare_paths(path, @path, env)
+        else
+          method == @method && path == @path
+        end
+      end
+
+      private
+
+      def compare_paths(path, route, env)
+        params, equal = compare_arrays(path.split('/'), route.split('/'), {}, 0)
+        env['simpler.params'] = params if equal
+      end
+
+      def compare_arrays(paths, routes, params, equal_count)
+        count = paths.size > routes.size ? paths.size : routes.size
+        for i in (0..count - 1)
+          if paths[i].to_i == 0
+            equal_count += 1 if paths[i] == routes[i]
+          elsif routes[i].include?(':')
+            equal_count += 1
+            params[routes[i].sub(':','').to_sym] = paths[i].to_i
+          end
+        end
+        return params, equal_count == count
       end
 
     end
   end
 end
+
+
